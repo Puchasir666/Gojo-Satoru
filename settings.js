4fd0d034698227b598062f717f8aaf4a2ffe6d88
Puchasir666 <105916135+Puchasir666@users.noreply.github.com>
2022-06-10 20:50:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['919961382049']
+global.premium = ['919961382049']
+global.ownernomer = '919961382049'
+global.ownername = '𝜑ͥ𝜓ͭ𝜁ᷤ𝛨ͫ͢𝛼ͤ𝜹ᤢ𝜄𝛾⸙⃝💖⃝࿙'
+global.botname = 'pucha-sir'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '𝜑ͥ𝜓ͭ𝜁ᷤ𝛨ͫ͢𝛼ͤ𝜹ᤢ𝜄𝛾⸙⃝💖⃝࿙'
+global.author = '𝜑ͥ𝜓ͭ𝜁ᷤ𝛨ͫ͢𝛼ͤ𝜹ᤢ𝜄𝛾⸙⃝💖⃝࿙'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
